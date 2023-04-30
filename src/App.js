53339de075e8c6bb884f66d177e365af86e4b663
abdelrahman <messelhy32@gmail.com>
2023-04-30 17:02:38 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { Col, Container, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import img from "./logo512.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="vw-100 vh-100 d-flex flex-column justify-content-center m-auto align-items-center">
+      <Form className="w-50">
+        <Row>
+          <Col>
+            <Form.Group className="mb-3" controlId="formGroupEmail">
+              <Form.Label className="text-primary">Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formGroupPassword">
+              <Form.Label className="text-primary">Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button>Sign In</Button>
+          </Col>
+          <Col className="m-auto text-center">
+            <img src={img} alt="" className="w-50 m-auto"/>
+          </Col>
+        </Row>
+      </Form>
+    </Container>
   );
 }
 
